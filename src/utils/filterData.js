bd0507f73c1f9a9a613085b504f68db5186536a8
LynnLai10 <52684000+LynnLai10@users.noreply.github.com>
2020-05-08 21:41:30 +0800

    fix week issue

--- src/utils/filterData.js
+++ src/utils/filterData.js
@@ -13,7 +13,7 @@ const filterData = (originalData, keys) => {
                     if ( i === 0 ) {
                         filteredDatum[key] = 'Today';
                     } else {
-                        filteredDatum[key] = dayOfWeek[moment.unix(data[key]).day()];
+                        filteredDatum[key] = moment.unix(data[key]).day();
                     }
                     break;
                 case 'temperature': 
